e5ef3f9db70d8b27712c80017553b1669b0068a2
Webbert <webbert.joji@gmail.com>
2021-01-29 15:12:19 +0800

    Added new formatting based on new fpredict feature

--- trading_bot.py
+++ trading_bot.py
@@ -1,5 +1,5 @@
 import argparse
-import crawler.fpredict as fp
+import Crawler.fpredict as fp
 import pandas as pd
 
 TIMECODES = ['1D', '5D', '1M', '6M', 'YTD', '1Y', '5Y', 'MAX']
@@ -8,24 +8,27 @@ TIMECODES = ['1D', '5D', '1M', '6M', 'YTD', '1Y', '5Y', 'MAX']
 def parse_arguments():
     parser = argparse.ArgumentParser(
         description="Trading prediction and bot tool")
-
-    parser.add_argument('-c', '--code', type=str,
-                        dest='code', help="Code based on YAHOO Finance.",
-                        required=True, metavar="")
+    m_group = parser.add_mutually_exclusive_group()
+    m_group.add_argument('-c', '--code', type=str,
+                         dest='code', help="Code based on YAHOO Finance.",
+                         metavar="", default=None)
+    m_group.add_argument('-f', '--file', type=str,
+                         dest='file', help='If CSV file present to be parsed.',
+                         default=None)
     parser.add_argument('-t', '--time',
                         dest="time", choices=TIMECODES,
                         help=f"The time spans.\nChoices are as followed: \
                             {TIMECODES}", metavar="")
     arg = parser.parse_args()
-    return arg.code, arg.time
+    return arg.code, arg.file, arg.time
 
 
 def main():
-    code, time = parse_arguments()
+    code, filename, time = parse_arguments()
     # Use CSV to retrieve info
     # df = pd.read_csv('LENDLEASE info max.csv')
-    x = fp.Crawl(code, time)
-    x.predict_(60)
+    x = fp.Crawl(code)
+    x.predict_()
 
 
 if __name__ == '__main__':
